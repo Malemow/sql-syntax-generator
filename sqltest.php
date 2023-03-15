7b0d4ff453688e006ee5b1e9cfc4aee2b1ebb945
Nicl.lin <nick.lin@cybersectech.net>
2023-03-15 14:19:48 +0800

    修正完畢

--- sqltest.php
+++ sqltest.php
@@ -1,15 +1,31 @@
 <?php
     include_once('./GenerateSQL.php');
 
-    /***** test 1 *****/
-
-    $sql = GenerateSQL::get_sql(
+    $sql_1 = GenerateSQL::get_sql(
         fields: '*',
         tables: 'test_1',
         wheres: [
-            "name" => sprintf("%s˚=˚AND", "Nick"),
-            "age" => sprintf("%d˚=˚", 5),
-        ]
+            "AND" => [
+                "status" => sprintf("%d˚=˚AND", 1),
+                "address" => sprintf("%s˚like˚", "台北市%")
+            ],
+            "name" => sprintf("%s˚=", "Nick"),
+        ],
+        between: [
+            "age" => sprintf("%d˚%d˚", 12, 18)
+        ],
+        groups: ["name", "age"],
+        orders: [
+            "desc" => ["address", "age"],
+            "asc" => ["bbb"]
+        ],
+        limits: [10 =>  20]
     );
 
-    var_dump($sql);
+    $test_tables = GenerateSQL::get_sql(
+        fields: '*',
+        tables: ['test1', sprintf("(%s)", $sql_1)]
+    );
+
+    var_dump($test_tables);
+
